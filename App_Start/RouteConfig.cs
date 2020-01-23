6b11d9c265ce8719fca0e7f0cc432b4573698eab
Jason Luckhardt <luckhaj@ad.nmc.edu>
2020-01-23 13:34:27 -0500

    Add project files.

---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MVCTestCompany
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Employee",
+                "Employee/{action}/{name}", //URL with parameter,
+                new { controller = "Employee", action = "search", name = UrlParameter.Optional } //parameter
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
